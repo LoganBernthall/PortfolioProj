6c2b936a7db2450c93f7bb394db9fd51c3f40b17
LoganBernthall <69209172+LoganBernthall@users.noreply.github.com>
2023-02-08 19:24:02 +0000

    4 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./style.css";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div class="headerBackground">
+      <div class="headerContent">
+        <h1 class="name"><b>Logan Elliottt Bernthall</b></h1>
+      </div>
     </div>
   );
 }
