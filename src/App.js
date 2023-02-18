f45b6d60432c5d97bbbcaffb5ee076f156110d8c
LoganBernthall <69209172+LoganBernthall@users.noreply.github.com>
2023-02-18 15:36:22 +0000

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import React from "react";
-import "./style.css";
-
+import React from 'react';
+import './style.css';
 
 export default function App() {
   return (
     <div>
-        <ul>
-        <li><a class="active" href="#home">Logan Elliott Bernthall</a></li>
-        <li><a href="#news" class="btnRight">News</a></li>
-        <li><a href="#contact" class="btnRight">Contact</a></li>
-        <li><a href="#about" class="btnRight">About</a></li>
-        </ul>     
+      <ul>
+        <li>
+          <a class="active" href="#home">
+            Logan Elliott Bernthall
+          </a>
+        </li>
+        <li>
+          <a href="#news" class="btnRight">
+            News
+          </a>
+        </li>
+        <li>
+          <a href="#contact" class="btnRight">
+            Contact
+          </a>
+        </li>
+        <li>
+          <a href="#about" class="btnRight">
+            About
+          </a>
+        </li>
+      </ul>
+      <div class="boxLeft">
+        
+      </div>
     </div>
-   
   );
 }
