ee5bdb47bf6532b44ed4958f483f92e77aeac160
LoganBernthall <69209172+LoganBernthall@users.noreply.github.com>
2023-02-25 19:50:12 +0000

    2 files modified, 1 added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Router, Switch, Route } from "react-router-dom";
+
+
 
 import App from './App';
+import Education from './Education';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <Router>
+    <Switch>  
+      <StrictMode>
+        <App />
+        <Education />
+      </StrictMode>
+    </Switch>
+  </Router>
 );
